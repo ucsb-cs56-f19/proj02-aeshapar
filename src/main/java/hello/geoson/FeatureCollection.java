b49385a76a169eb27d9d5f86f73fcf878388a31d
aeshaparekh@ucsb.edu <aeshaparekh@csil-20.cs.ucsb.edu>
2019-11-15 19:19:51 -0800

    create metadata and display it

--- src/main/java/hello/geoson/FeatureCollection.java
+++ src/main/java/hello/geoson/FeatureCollection.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class FeatureCollection {
    private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
    public String type;
+   public Metadata metadata;
 
    /**
      * Create a FeatureCollection object from json representation
