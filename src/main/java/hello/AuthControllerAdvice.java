a35ca8f5e692b1646cf5d8fba60d447c324bee03
aeshaparekh@ucsb.edu <aeshaparekh@csil-43.cs.ucsb.edu>
2019-11-22 15:08:34 -0800

    added users table

--- src/main/java/hello/AuthControllerAdvice.java
+++ src/main/java/hello/AuthControllerAdvice.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import hello.repositories.UserRepository;
+import hello.entities.AppUser;
+import java.util.List;
 
 @ControllerAdvice
 public class AuthControllerAdvice {
 
-    @Autowired   
+    @Autowired
     private MembershipService membershipService;
+    @Autowired
+    private UserRepository userRepository;
 
     @ModelAttribute("isLoggedIn")
     public boolean getIsLoggedIn(OAuth2AuthenticationToken token){
@@ -19,14 +24,26 @@ public class AuthControllerAdvice {
 
     @ModelAttribute("id")
     public String getUid(OAuth2AuthenticationToken token){
-        if (token == null) return "";
-        return token.getPrincipal().getAttributes().get("id").toString();
-    }
+       if (token == null) return "";
+
+       String uid = token.getPrincipal().getAttributes().get("id").toString();
+
+       List<AppUser> users = userRepository.findByUid(uid);
+
+       if (users.size()==0) {
+           AppUser u = new AppUser();
+           u.setUid(uid);
+           u.setLogin(token2login(token));
+           userRepository.save(u);
+       }
+
+       return uid;
+   }
 
     @ModelAttribute("login")
     public String getLogin(OAuth2AuthenticationToken token){
         if (token == null) return "";
-        return token.getPrincipal().getAttributes().get("login").toString();
+        return token2login(token);
     }
 
     @ModelAttribute("isMember")
@@ -41,5 +58,9 @@ public class AuthControllerAdvice {
     @ModelAttribute("role")
     public String getRole(OAuth2AuthenticationToken token){
         return membershipService.role(token);
+    }
+
+    private String token2login(OAuth2AuthenticationToken token) {
+        return token.getPrincipal().getAttributes().get("login").toString();
     }
 }
