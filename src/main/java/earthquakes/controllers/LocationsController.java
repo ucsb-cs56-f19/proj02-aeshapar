cad00c85d7177adf5561e0b367515bbd8bb77519
aeshaparekh@ucsb.edu <aeshaparekh@csil-23.cs.ucsb.edu>
2019-11-28 10:58:12 -0800

    added location search form

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -1,6 +1,6 @@
 package earthquakes.controllers;
 import earthquakes.services.EarthquakeQueryService;
-import earthquakes.searches.EqSearch;
+import earthquakes.searches.LocSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -17,26 +17,26 @@ import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 import earthquakes.geojson.FeatureCollection;
 
 @Controller
-public class EarthquakesController {
+public class LocationsController {
 
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
 
-    @GetMapping("/earthquakes/search")
-    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-            EqSearch eqSearch) {
-        return "earthquakes/search";
+    @GetMapping("/locations/search")
+    public String getLocationsSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            LocSearch locSearch) {
+        return "/locations/search";
     }
 
-    @GetMapping("/earthquakes/results")
+    @GetMapping("/locations/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-          EqSearch eqSearch) {
-          EarthquakeQueryService e = new EarthquakeQueryService();
-          model.addAttribute("eqSearch", eqSearch);
-          String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
-          model.addAttribute("json", json);
-          FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
-          model.addAttribute("featureCollection",featureCollection);
-          return "earthquakes/results";
+          LocSearch locSearch) {
+          // EarthquakeQueryService e = new EarthquakeQueryService();
+          // model.addAttribute("locSearch", locSearch);
+          // String json = e.getJSON(locSearch.getLocation());
+          // model.addAttribute("json", json);
+          // FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+          // model.addAttribute("featureCollection",featureCollection);
+          return "locations/results";
     }
 }
